d17d7262c2821375b679bd61c3ba0fae114dd37d
giupezzali <pez21001@byui.edu>
2022-02-05 01:31:41 -0300

    js changes

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,21 +1,21 @@
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-const list = document.querySelector("list");
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('.list');
 
-button.addEventListener("click", function() {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
+button.addEventListener('click', function() {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
     li.textContent = input.value;
-    btn.textContent = "❌";
+    btn.textContent = '❌';
     li.append(btn);
     list.append(li);
-    button.addEventListener("click", deleteLi);
+    button.addEventListener('click', deleteLi());
     input.focus();
-
+    input.value = '';
 });
 
 function deleteLi() {
-    const parentli = (this).parentNode.remove();
+    const parentLi = (this).parentNode.remove();
     input.focus();
 }
 
