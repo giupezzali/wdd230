3fb6701c272701d2ef8aedad92e11297db10b00d
giupezzali <pez21001@byui.edu>
2022-02-19 15:16:09 -0300

    fixes

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -22,3 +22,13 @@ document.querySelector('#year').textContent = currentDate.getFullYear();
 let oLastModif = document.lastModified;
 document.getElementById('updated').innerHTML = oLastModif;
 
+const weekDay = now.getDay();
+
+let banner = document.querySelector('#banner')
+
+if (weekDay == 1 || weekDay == 2) {
+    banner.style.display = 'block';
+}
+else {
+    banner.style.display = 'none';
+}
