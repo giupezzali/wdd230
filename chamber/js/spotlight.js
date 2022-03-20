01fb87ac7cea5e7bccb3eccd7ec8bb1100e78bd4
giupezzali <pez21001@byui.edu>
2022-03-20 01:24:10 -0300

    last modification

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -9,17 +9,18 @@ fetch(requestURL)
         console.table(jsonObject);
         const companies = jsonObject['companies'];
       let membershiFiltered = companies.filter(membership => membership.membership === 'gold' || membership.membership === 'silver');
+      console.log(membershiFiltered);
   
       for (i = 1; i < 4; i++) {
         const randomMember =[Math.floor(Math.random()*membershiFiltered.length)];
-        displayCompanies(membershiFiltered[randomMember], `.spotlight${i}`);
+        displayCompanies(membershiFiltered[randomMember]);
     }
 });
        
 
-function displayCompanies(company, spotlight) {
-    let card = document.querySelector(spotlight);
+function displayCompanies(company) {
     let logo = document.createElement('img');
+    let address = document.createElement('p');
     let phone = document.createElement('p');
     let website = document.createElement('p');
 
@@ -27,10 +28,12 @@ function displayCompanies(company, spotlight) {
     logo.setAttribute('alt', `${company.name}`);
     logo.setAttribute('loading', 'lazy');
 
+    address.textContent = `${company.address}`;
     phone.textContent = `${company.phone}`;
     website.innerHTML = `<a target="_blank" href="#">${company.website}</a>`;
 
-    card.appendChild(logo);
-    card.appendChild(phone);
-    card.appendChild(website);
+    cards.appendChild(logo);
+    cards.appendChild(address);
+    cards.appendChild(phone);
+    cards.appendChild(website);
 }
