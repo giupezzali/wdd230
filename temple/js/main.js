8efcf5d976e2cf4dc757975beb9a4d4d810a3adc
giupezzali <pez21001@byui.edu>
2022-03-29 23:33:28 -0300

    Dont try using grid and button EVER again!

--- temple/js/main.js
+++ temple/js/main.js
@@ -1,12 +1,3 @@
-//Create hamburger Button
-function toggleMenu() {
-    document.getElementById("navigation").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-}
-
-const x = document.getElementById('hamburgerBtn')
-x.onclick = toggleMenu;
-
 //current year in footer -------------------
 const currentDate = new Date();
 document.querySelector('#year').textContent = currentDate.getFullYear();
@@ -14,5 +5,14 @@ document.querySelector('#year').textContent = currentDate.getFullYear();
 //last modification--------------------------
 let oLastModif = document.lastModified;
 document.getElementById('updated').innerHTML = oLastModif;
+
+
+//Navigation Bar -----------------------------
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 
